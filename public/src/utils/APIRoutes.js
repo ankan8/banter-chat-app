2041e45c23d9dd131090f989484bcf8f9bb8d34b
ankan8 <154518866+ankan8@users.noreply.github.com>
2024-09-23 00:50:24 +0530

    Update APIRoutes.js

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,9 +1,13 @@
-export const host = "http://localhost:5000";
-export const registerRoute= `${host}/api/auth/register`
+export const host = "https://banter-chat-app-api.vercel.app";
 
-export const setAvatarRoute= `${host}/api/auth/setAvatar`
-export const allUsersRoute = `${host}/api/auth/allusers`
-export const sendMessageRoute = `${host}/api/messages/addmsg`
-export const getAllMessagesRoute = `${host}/api/messages/getmsg`
+// Auth routes
+export const registerRoute = `${host}/api/auth/register`;
+export const setAvatarRoute = `${host}/api/auth/setAvatar`;
+export const allUsersRoute = `${host}/api/auth/allusers`;
 
-export const loginRoute = 'https://banter-chat-app-api.vercel.app/login';
+// Message routes
+export const sendMessageRoute = `${host}/api/messages/addmsg`;
+export const getAllMessagesRoute = `${host}/api/messages/getmsg`;
+
+// Login route
+export const loginRoute = `${host}/login`;
